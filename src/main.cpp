58b7fd6f84079bb4f2fb9adf86cad7f7df01acdd
Zachary Donsky <zachary.donsky@achievers.com>
2021-10-21 22:38:11 -0400

    updated project in guislice editor with confirmed existing font fron tftespi

--- src/main.cpp
+++ src/main.cpp
@@ -12,31 +12,98 @@
 // ------------------------------------------------
 // Headers to include
 // ------------------------------------------------
-#include "Arduino.h"
 #include "GUIsliceProjects_GSLC.h"
 
 
 // ------------------------------------------------
 // Program Globals
 // ------------------------------------------------
-#define MAX_STR 15
 
-// Save some element references for quick access
-gslc_tsElemRef *m_pElemCnt = NULL;
-gslc_tsElemRef *m_OCCUPANCY = NULL;
-gslc_tsElemRef *m_pElemVal2 = NULL;
-gslc_tsElemRef *m_pElemXRingGauge1 = NULL;
-gslc_tsElemRef *m_pElemKeyPadNum = NULL;
+// Save some element references for direct access
+//<Save_References !Start!>
+gslc_tsElemRef* m_pElemVal2       = NULL;
+gslc_tsElemRef* m_pElemXRingGauge1= NULL;
+gslc_tsElemRef* m_pElemKeyPadNum  = NULL;
+//<Save_References !End!>
+
 // Define debug message function
 static int16_t DebugOut(char ch) { if (ch == (char)'\n') Serial.println(""); else Serial.write(ch); return 0; }
 
+// ------------------------------------------------
+// Callback Methods
+// ------------------------------------------------
+// Common Button callback
+bool CbBtnCommon(void* pvGui,void *pvElemRef,gslc_teTouch eTouch,int16_t nX,int16_t nY)
+{
+  // Typecast the parameters to match the GUI and element types
+  gslc_tsGui*     pGui     = (gslc_tsGui*)(pvGui);
+  gslc_tsElemRef* pElemRef = (gslc_tsElemRef*)(pvElemRef);
+  gslc_tsElem*    pElem    = gslc_GetElemFromRef(pGui,pElemRef);
+
+  if ( eTouch == GSLC_TOUCH_UP_IN ) {
+    // From the element's ID we can determine which button was pressed.
+    switch (pElem->nId) {
+//<Button Enums !Start!>
+      case CAPACITY_NUM_MONITOR:
+        // Clicked on edit field, so show popup box and associate with this text field
+        gslc_ElemXKeyPadInputAsk(&m_gui, m_pElemKeyPadNum, E_POP_KEYPAD_NUM, m_pElemVal2);
+        break;
+//<Button Enums !End!>
+      default:
+        break;
+    }
+  }
+  return true;
+}
+//<Checkbox Callback !Start!>
+//<Checkbox Callback !End!>
+// KeyPad Input Ready callback
+bool CbKeypad(void* pvGui, void *pvElemRef, int16_t nState, void* pvData)
+{
+  gslc_tsGui*     pGui     = (gslc_tsGui*)pvGui;
+  gslc_tsElemRef* pElemRef = (gslc_tsElemRef*)(pvElemRef);
+  gslc_tsElem*    pElem    = gslc_GetElemFromRef(pGui,pElemRef);
+
+  // From the pvData we can get the ID element that is ready.
+  int16_t nTargetElemId = gslc_ElemXKeyPadDataTargetIdGet(pGui, pvData);
+  if (nState == XKEYPAD_CB_STATE_DONE) {
+    // User clicked on Enter to leave popup
+    // - If we have a popup active, pass the return value directly to
+    //   the corresponding value field
+    switch (nTargetElemId) {
+//<Keypad Enums !Start!>
+      case CAPACITY_NUM_MONITOR:
+        gslc_ElemXKeyPadInputGet(pGui, m_pElemVal2, pvData);
+	    gslc_PopupHide(&m_gui);
+        break;
+
+//<Keypad Enums !End!>
+      default:
+        break;
+    }
+  } else if (nState == XKEYPAD_CB_STATE_CANCEL) {
+    // User escaped from popup, so don't update values
+    gslc_PopupHide(&m_gui);
+  }
+  return true;
+}
+//<Spinner Callback !Start!>
+//<Spinner Callback !End!>
+//<Listbox Callback !Start!>
+//<Listbox Callback !End!>
+//<Draw Callback !Start!>
+//<Draw Callback !End!>
+//<Slider Callback !Start!>
+//<Slider Callback !End!>
+//<Tick Callback !Start!>
+//<Tick Callback !End!>
 
 void setup()
 {
   // ------------------------------------------------
   // Initialize
   // ------------------------------------------------
-  Serial.begin(115200);
+  Serial.begin(9600);
   // Wait for USB Serial 
   //delay(1000);  // NOTE: Some devices require a delay after Serial.begin() before serial port can be used
 
@@ -46,39 +113,25 @@ void setup()
   // Create graphic elements
   // ------------------------------------------------
   InitGUIslice_gen();
-  // gslc_ElemXRingGaugeSetValRange(&m_gui, pElemRef, 0, CAPACITY);
+
 }
 
 // -----------------------------------
 // Main event loop
 // -----------------------------------
-int dial_tick = 0;
-unsigned int now, dial_tick_interval;
 void loop()
 {
-  now = millis();
-  if (now - dial_tick_interval >= 200)
-  {
-    dial_tick++;
-    dial_tick_interval=now;
-  } else if (dial_tick >= 100)
-  {
-    dial_tick=0;
-  }
-  
 
   // ------------------------------------------------
   // Update GUI Elements
   // ------------------------------------------------
-  char dial_tick_string[MAX_STR];
-  snprintf(dial_tick_string, MAX_STR, "%u", dial_tick);
-  gslc_ElemSetTxtStr(&m_gui, m_OCCUPANCY, dial_tick_string);
-  gslc_ElemXRingGaugeSetVal(&m_gui, m_pElemXRingGauge1, dial_tick);
+  
   //TODO - Add update code for any text, gauges, or sliders
-
+  
   // ------------------------------------------------
   // Periodically call GUIslice update function
   // ------------------------------------------------
   gslc_Update(&m_gui);
+    
 }
 
